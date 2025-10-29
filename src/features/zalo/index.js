ce5e52f557a5e88eae84949d55d113e60b042b05
RFS-ADRENO <dungto76@gmail.com>
2025-10-29 17:15:12 +0700

    feat: implement keep-alive functionality for Zalo API session

--- src/features/zalo/index.js
+++ src/features/zalo/index.js
@@ -5,30 +5,47 @@ class ZaloAPI {
      * @type {ZaloAPI|null}
      */
     static instance = null;
-    
+
+    #keepAliveInterval = null;
+
     constructor() {
         if (ZaloAPI.instance) {
             return ZaloAPI.instance;
         }
-        
+
         this.api = null;
         ZaloAPI.instance = this;
     }
-    
+
     static getInstance() {
         if (!ZaloAPI.instance) {
             ZaloAPI.instance = new ZaloAPI();
         }
         return ZaloAPI.instance;
     }
-    
+
     async startSession() {
         this.api = await loginToZalo();
+
+        if (this.#keepAliveInterval) {
+            clearInterval(this.#keepAliveInterval);
+        }
+
+        this.#keepAliveInterval = setInterval(() => {
+            this.keepAlive();
+        }, 2 * 60 * 1000); // every 2 minutes
+
         return this.api;
     }
-    
+
     getAPI() {
         return this.api;
+    }
+
+    keepAlive() {
+        if (this.api) {
+            this.api.keepAlive().catch(console.error);
+        }
     }
 }
 
